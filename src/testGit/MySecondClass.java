1cfee9a3de89853968c68258f1dd3ca71ec01b07
kpono <kpono@10.44.175.120>
2016-02-02 13:54:09 -0500

    we added MySecondClass.java

---
+++ src/testGit/MySecondClass.java
@@ -0,0 +1,14 @@
+package testGit;
+
+public class MySecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * this class is cool
+		 */
+		
+
+	}
+
+}
